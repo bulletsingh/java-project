33dfe2136bcc285f65c0d75479b7d219e4a9df5a
Navdeep Singh <navisahi77@gmail.com>
2018-09-24 20:01:54 +0000

    Initail Java Commit

---
+++ src/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle{
+	public int length;
+	public int width;
+	public(int length, int width){
+	this.length = length;
+	this.width = width;
+}
+
+	public int getArea(){
+	return length*width;
+}
+	public int getParimeter(){
+	return 2*(length + width);
+}
+}
