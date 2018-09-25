6d5f4a1977b9c807a3d8cd7580e32f272f18cc77
Navdeep Singh <navisahi77@gmail.com>
2018-09-25 20:42:50 +0000

    modifying rectangular file

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,11 +1,12 @@
-public class Rectangulator{
-	public static void main(String[] args){
-	int length = Integer.parseInt(args[0]);
-	int width = Integer.parseInt(args[1]);
+public class Rectangulator {
+  public static void main(String[] args) {
+    int length = Integer.parseInt(args[0]);
+    int width = Integer.parseInt(args[1]);
 
-	Rectangle myrectangle = new Rectangle(length, width);
-	String output = String.format("*** Your Rectangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+    Rectangle myRectangle = new Rectangle(length, width);
 
-	System.out.println(output);
-}
+    String output = String.format("*** Your Rectangle ***\n\nLength: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+
+    System.out.println(output);
+  }
 }
